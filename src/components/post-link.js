94c09778cc424a3e37ce587ddaac0223413b801a
Liam Kiniry <46054023+LiamArcherKiniry@users.noreply.github.com>
2020-05-20 11:19:42 -0400

    Changing to a class component + changing styling
    changing to a class component so I can include post components as variables in props

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -1,21 +1,54 @@
-import React from "react"
+import React, { Component } from 'react';
 import { Link } from "gatsby"
 
-const PostLink = ({ post }) => (
-  <article className="card ">
-    <Link to={post.frontmatter.path}>
-      {!!post.frontmatter.thumbnail && (
-        <img src={post.frontmatter.thumbnail} alt={post.frontmatter.title + "- Featured Shot"} />
-      )}
-    </Link>
-    <header>
-      <h2 className="post-title">
-        <Link to={post.frontmatter.path} className="post-link">
-          {post.frontmatter.title}
-        </Link>
-      </h2>
-      <div className="post-meta">{post.frontmatter.date}</div>
-    </header>
-  </article>
-)
-export default PostLink
+
+/* const PostLink = ({ post }) => (
+  <article id={post.frontmatter.title} className="c-services__item">
+    <div className="c-services__item_inner">
+        <div title={post.frontmatter.title + "- illustrated icon"} id={post.frontmatter.path + "-anim"} className="shapeshifter" ></div>
+        <h3>{post.frontmatter.title}</h3>
+        <p>{post.frontmatter.description}</p>
+            <Link to={post.frontmatter.path} className="after">
+              
+            </Link>
+    </div>
+    </article>
+
+
+
+
+
+    
+) */
+
+class Postlink extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.sectionStyle = {
+      backgroundImage: `url(${this.props.post.frontmatter.thumbnail})`
+    };
+ 
+  };
+  
+
+  render() {
+    return(
+      <article id={this.props.post.frontmatter.title} className="c-services__item">
+      <div className="c-services__item_inner">
+          <div title={this.props.post.frontmatter.title + "- illustrated icon"} id={this.props.post.frontmatter.path + "-anim"} className="shapeshifter" style={ this.sectionStyle }></div>
+          <h3>{this.props.post.frontmatter.title}</h3>
+          <p>{this.props.post.frontmatter.description}</p>
+              <Link to={this.props.post.frontmatter.path} className="after">
+                
+              </Link>
+      </div>
+      </article>
+  
+    )
+  }
+  
+}
+
+
+export default Postlink;

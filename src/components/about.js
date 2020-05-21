46053efced59e78087f22519585ff16f012cb961
Liam Kiniry <lak6gb@g.rit.edu>
2020-05-21 08:59:23 -0400

    changed heading text styling

--- src/components/about.js
+++ src/components/about.js
@@ -19,7 +19,7 @@ class About extends Component {
         <div id="about" className="about" style={ this.aboutStyle }>
         <div className="prod-left">
             <div className="prod-hcontent">
-            <h2 className="head-works">About</h2>
+            <h2 className="prod-head">About</h2>
             <p className="">I'm a UX designer and developer passionate about people, design, and health. Many of my projects have been about making data more accessible and easier for people to explore and interact with. I hope my studies can lead me toward developing knowledge rich experiences for people of all needs. In my free time, you can find doodling, petting my dog, and drinking iced coffee in the dead of winter.</p>
             </div>
         </div>
